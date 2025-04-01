4ccff602cbb63e6182fe84c9057677a308f37fca
Sophia <sophialimasantos4@gmail.com>
2025-04-01 19:33:25 -0300

    finalizando aula1

--- aula1/script.js
+++ aula1/script.js
@@ -2,4 +2,4 @@ quantEuros = prompt("Digite em euro: ")
 
 uniEuro = 6.17; //em reais
 euroEmReais = quantEuros * uniEuro
-alert(quantEuros + " wons equivale a " + euroEmReais + " reais")
+alert(quantEuros + " euros equivale a " + euroEmReais + " reais")
